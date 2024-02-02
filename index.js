551a3a804062bc19de26ec6434fead5e03b7b2a1
Brendan Dempsey <bjd1412@gmail.com>
2024-02-02 04:11:17 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const body = document.querySelector('body')
+
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+    "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name,
+        email,
+    })
+    
+})
+.then(res => res.json())
+    .then(userInfo => {
+        body.innerHTML = userInfo.id
+    })
+    .catch(error => {
+        body.innerHTML = error.message
+    } )
+}
